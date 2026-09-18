a4d3dfa7ed6d65c1b0fa267b26acf64daf3a130b
sangour <149207012+RayenMneja@users.noreply.github.com>
2026-09-18 21:18:45 +0200

    Change directory to klyvo-dashboard
    change path

--- klyvo.bat
+++ klyvo.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "E:\klyvo-admin-dashboard-fixed\klyvo-admin-dashboard-fixed"
+cd /d "klyvo-dashboard"
 start /B node --env-file=.env server/index.mjs
 timeout /t 3 /nobreak > NUL
 start http://localhost:3000
